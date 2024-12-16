bbb82feaaa87fd94c85dd18ae998fa8bfc95f396
edutcardo <100849361+edutcardo@users.noreply.github.com>
2024-12-16 14:38:15 -0300

    Adicionando automação de atualizar API

---
+++ att.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Executar Importação de Dados</title>
+</head>
+<body>
+    <form action="save_data.php" method="post">
+        <button type="submit" name="execute">Executar Importação</button>
+    </form>
+</body>
+</html>
