1144836b14a4e285d36f31224047c21874b1f1b2
edutcardo <100849361+edutcardo@users.noreply.github.com>
2025-02-11 17:13:57 -0300

    +2 novos inputs adicionados

--- inputAutomatico.php
+++ inputAutomatico.php
@@ -34,24 +34,31 @@ if (!empty($inputConcessionaria) && !empty($usina)) {
         // Calcula de acordo com o tipo de usina
         switch($usina) {
             case '75kwSolo':
+                $precoFinalBack = 298920.95;
                 $resultado = $compensavel * 12768;
                 break;
             case '75kwTelhado':
+                $precoFinalBack = 264798.28;
                 $resultado = $compensavel * 12804;
                 break;
             case '300kwSolo':
+                $precoFinalBack = 1270002.81;
                 $resultado = $compensavel * 51072;
                 break;
             case '300kwTelhado':
+                $precoFinalBack = 1043683.57;
                 $resultado = $compensavel * 51217;
                 break;
             case '1mwSolo':
+                $precoFinalBack = 4891468.81;
                 $resultado = $compensavel * 170772;
                 break;
             case '1mwTelhado':
+                $precoFinalBack = 4390447.08;
                 $resultado = $compensavel * 166458;
                 break;
             default:
+                $precoFinalBack = "Não foi possível calcular";
                 $resultado = "Valor de usina desconhecido";
                 break;
         }
@@ -59,16 +66,23 @@ if (!empty($inputConcessionaria) && !empty($usina)) {
         // Calcula o retorno anual
         if (is_numeric($resultado)) {
             $retornoVerdeAnual = $resultado * 12;
+            $rentabilidadeVerde = $precoFinalBack / ($resultado * 12);
             
             $resultado_formatado = 'R$ ' . number_format($resultado, 2, ',', '.');
             $retornoVerdeAnual_formatado = 'R$ ' . number_format($retornoVerdeAnual, 2, ',', '.');
+            $precoFinalBack_formatado = 'R$ ' . number_format($precoFinalBack, 2, ',', '.');
+            $rentabilidadeVerde_formatado = number_format($rentabilidadeVerde * 100, 2, ',', '.') . '%';
+
         } else {
             $resultado_formatado = $resultado;
             $retornoVerdeAnual_formatado = $resultado;
+            $precoFinalBack_formatado = $precoFinalBack;
         }
+
+
         
         // Envie os resultados formatados como resposta, separados por algum delimitador, por exemplo "|"
-        echo $resultado_formatado . "|" . $retornoVerdeAnual_formatado;
+        echo $resultado_formatado . "|" . $retornoVerdeAnual_formatado . "|" . $precoFinalBack_formatado . "|" . $rentabilidadeVerde_formatado;
     
     } else {
         echo "";
